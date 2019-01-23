6e51307bbaa38137044af9451d4ce1f70de56da9
Alireza <aryadesignco@gmail.com>
2019-01-23 20:03:36 +0330

    Menu, Slider Added

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$(".sliders").slick({
+    rtl: true,
+    dots: true,
+    nextArrow: "<button class='arrow next'><i class='fa fa-angle-right'></i></button>",
+    prevArrow: "<button class='arrow prev'><i class='fa fa-angle-left'></i></button>"
+})
